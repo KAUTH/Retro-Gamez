1d7bf398f5f8c95938facb74d7ba39eac8f13188
DPAUTH <dphp76@gmail.com>
2020-08-11 02:45:08 +0300

    Create List of Games
    This patch adds the currently most popular games of retrogames.cc
    (total of 24 games).

--- retro-gamez/popup/game_list.js
+++ retro-gamez/popup/game_list.js
@@ -8,5 +8,117 @@ var gameList = [
 		"id": 2,
 		"title": "Super Mario Kart (USA)",
 		"src": "https://www.retrogames.cc/embed/18635-super-mario-kart-usa-hack-by-ok-impala-v1-0-super-mario-kart-the-impala-battles-custom-battle-tracks.html"
+	},
+	{
+		"id": 3,
+		"title": "Super Mario Bros. (Europe) (Rev 0A)",
+		"src": "https://www.retrogames.cc/embed/21929-super-mario-bros-europe-rev-0a.html"
+	},
+	{
+		"id": 4,
+		"title": "Sonic the Hedgehog 2 (World) (Rev A)",
+		"src": "https://www.retrogames.cc/embed/17236-sonic-the-hedgehog-2-world-rev-a.html"
+	},
+	{
+		"id": 5,
+		"title": "Dragon Ball Z - Supersonic Warriors (K)(ProjectG)",
+		"src": "https://www.retrogames.cc/embed/28096-dragon-ball-z-supersonic-warriors-k-projectg.html"
+	},
+	{
+		"id": 6,
+		"title": "Tekken 3",
+		"src": "https://www.retrogames.cc/embed/40238-tekken-3.html"
+	},
+	{
+		"id": 7,
+		"title": "Marvel vs Capcom - clash of super heroes (980123 USA)",
+		"src": "https://www.retrogames.cc/embed/9123-marvel-vs-capcom-clash-of-super-heroes-980123-usa.html"
+	},
+	{
+		"id": 8,
+		"title": "Super Mario 64 (USA)",
+		"src": "https://www.retrogames.cc/embed/32112-super-mario-64-usa.html"
+	},
+	{
+		"id": 9,
+		"title": "Super Mario All-Stars (USA)",
+		"src": "https://www.retrogames.cc/embed/16886-super-mario-all-stars-usa.html"
+	},
+	{
+		"id": 10,
+		"title": "JoJo's Bizarre Adventure: Heritage for the Future / JoJo no Kimyou na Bouken: Mirai e no Isan (Japan 990927, NO CD)",
+		"src": "https://www.retrogames.cc/embed/8843-jojos-bizarre-adventure%3A-heritage-for-the-future-jojo-no-kimyou-na-bouken%3A-mirai-e-no-isan-japan-990927-no-cd.html"
+	},
+	{
+		"id": 11,
+		"title": "Super Mario World - Super Mario Advance 2 (J)(Eurasia)",
+		"src": "https://www.retrogames.cc/embed/28605-super-mario-world-super-mario-advance-2-j-eurasia.html"
+	},
+	{
+		"id": 12,
+		"title": "Pokemon Rouge Feu (F)(Rising Sun)",
+		"src": "https://www.retrogames.cc/embed/18861-pokemon-rouge-feu-f-rising-sun.html"
+	},
+	{
+		"id": 13,
+		"title": "Super Bomberman 4 (English - Translated)",
+		"src": "https://www.retrogames.cc/embed/19846-super-bomberman-4-english-translated.html"
+	},
+	{
+		"id": 14,
+		"title": "Pokemon Emerald (U)(TrashMan)",
+		"src": "https://www.retrogames.cc/embed/19514-pokemon-emerald-u-trashman.html"
+	},
+	{
+		"id": 15,
+		"title": "Super Mario Bros. (Japan, USA)",
+		"src": "https://www.retrogames.cc/embed/16843-super-mario-bros-japan-usa.html"
+	},
+	{
+		"id": 16,
+		"title": "Street Fighter II' - Champion Edition (YYC bootleg set 2, 920313 etc) [Bootleg]",
+		"src": "https://www.retrogames.cc/embed/10042-street-fighter-ii-champion-edition-yyc-bootleg-set-2-920313-etc-bootleg.html"
+	},
+	{
+		"id": 17,
+		"title": "Super Smash Bros. (USA)",
+		"src": "https://www.retrogames.cc/embed/32117-super-smash-bros-usa.html"
+	},
+	{
+		"id": 18,
+		"title": "Mario Kart 64 (E)",
+		"src": "https://www.retrogames.cc/embed/32109-mario-kart-64-e.html"
+	},
+	{
+		"id": 19,
+		"title": "Sonic the Hedgehog 3 (Europe)",
+		"src": "https://www.retrogames.cc/embed/30002-sonic-the-hedgehog-3-europe.html"
+	},
+	{
+		"id": 20,
+		"title": "Super Mario Kart (USA)",
+		"src": "https://www.retrogames.cc/embed/16887-super-mario-kart-usa.html"
+	},
+	{
+		"id": 21,
+		"title": "Super Mario Bros. 3 (USA)",
+		"src": "https://www.retrogames.cc/embed/16680-super-mario-bros-3-usa.html"
+	},
+	{
+		"id": 22,
+		"title": "Neo Bomberman",
+		"src": "https://www.retrogames.cc/embed/9293-neo-bomberman.html"
+	},
+	{
+		"id": 23,
+		"title": "Sonic & Knuckles + Sonic the Hedgehog 3 (World)",
+		"src": "https://www.retrogames.cc/embed/29209-sonic-knuckles-sonic-the-hedgehog-3-world.html"
+	},
+	{
+		"id": 24,
+		"title": "Sonic the Hedgehog (USA, Europe) [Hack by LOst v1.0] (Sonic Jam's Easy Mode)",
+		"src": "https://www.retrogames.cc/embed/28249-sonic-the-hedgehog-usa-europe-hack-by-lost-v1-0-sonic-jam-s-easy-mode.html"
 	}
+
+	
 ]
