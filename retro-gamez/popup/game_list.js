8197d1b808b64886a2c90ca9072b615ec211c98d
KAUTH <konpap1996@yahoo.com>
2020-08-10 23:02:42 +0300

    Local Implementation
    In this patch we add the list of games in a
    JS file that is stored locally with the rest
    of the source code.

---
+++ retro-gamez/popup/game_list.js
@@ -0,0 +1,12 @@
+var gameList = [
+	{
+		"id": 1,
+		"title": "Super Mario World (USA)",
+		"src": "https://www.retrogames.cc/embed/16847-super-mario-world-usa.html"
+	},
+	{
+		"id": 2,
+		"title": "Super Mario Kart (USA)",
+		"src": "https://www.retrogames.cc/embed/18635-super-mario-kart-usa-hack-by-ok-impala-v1-0-super-mario-kart-the-impala-battles-custom-battle-tracks.html"
+	}
+]
--- retro-gamez/popup/retro_gamez.js
+++ retro-gamez/popup/retro_gamez.js
@@ -12,12 +12,8 @@ function dayOfYear() {
 	return day;
 }
 
-// An array of objects containing the available games
-var games = [
-	{id:1, title:"Super Mario World (USA)", src:"https://www.retrogames.cc/embed/16847-super-mario-world-usa.html"},
-	{id:2, title:"Super Mario Kart (USA)", src:"https://www.retrogames.cc/embed/18635-super-mario-kart-usa-hack-by-ok-impala-v1-0-super-mario-kart-the-impala-battles-custom-battle-tracks.html"}
-];
-
+// Getting the array of objects containing the available games from a local JS file, stored in the gameList variable
+var games = gameList;
 var totalGames = Object.keys(games).length;
 
 // Get the date, specifically the day as a number
